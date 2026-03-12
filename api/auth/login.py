f4b69f26ee925ce0468358cceebd22c8f751758c
yapplytr-cmyk <yapplytr@gmail.com>
2026-03-12 21:13:04 +0300

    Fix live auth API handlers

--- api/auth/login.py
+++ api/auth/login.py
@@ -1,8 +1,8 @@
 from http.server import BaseHTTPRequestHandler
 
-from api._utils import handle_login, run_api_action
+from api.supabase_utils import handle_public_login, run_supabase_action
 
 
 class handler(BaseHTTPRequestHandler):
   def do_POST(self):
-    run_api_action(self, handle_login)
+    run_supabase_action(self, handle_public_login)
--- api/auth/signup.py
+++ api/auth/signup.py
@@ -1,8 +1,8 @@
 from http.server import BaseHTTPRequestHandler
 
-from api._utils import handle_signup, run_api_action
+from api.supabase_utils import handle_public_signup, run_supabase_action
 
 
 class handler(BaseHTTPRequestHandler):
   def do_POST(self):
-    run_api_action(self, handle_signup)
+    run_supabase_action(self, handle_public_signup)
